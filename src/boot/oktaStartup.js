d7e964319a108af8e0289c5fc5281f37bd211129
tore gard <dragerot@gmail.com>
2020-05-26 19:35:50 +0200

    Added stromapp test

--- src/boot/oktaStartup.js
+++ src/boot/oktaStartup.js
@@ -3,7 +3,7 @@ import Auth from '@okta/okta-vue'
 export default async ({ router, Vue }) => {
   Vue.use(Auth, {
     issuer: 'https://dev-207979.okta.com/oauth2/default',
-    clientId: '0oaaoxkde6Rv10fXu4x6',
+    clientId: '0oad4rqh0y74pL9DC4x6',
     redirectUri: window.location.origin + '/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true
